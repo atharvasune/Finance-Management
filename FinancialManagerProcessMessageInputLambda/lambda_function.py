b726f283eafe2239151753b3e4bb74507c03c9a2
Atharva Sune <atharvasune@gmail.com>
2025-05-17 19:29:58 +0530

    update lambda function code

--- FinancialManagerProcessMessageInputLambda/lambda_function.py
+++ FinancialManagerProcessMessageInputLambda/lambda_function.py
@@ -4,5 +4,5 @@ from aws_lambda_typing import context as context_, events
 def lambda_handler(event: events.APIGatewayProxyEventV2, context):
     return {
         'statusCode': 200,
-        'body': event.body
+        'body': event["body"]
     }
